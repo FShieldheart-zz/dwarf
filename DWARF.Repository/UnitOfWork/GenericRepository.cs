fd2a9a956dc1d1ac15c379e221f99959450fdf57
FShieldheart <namikbarisidil@hotmail.com>
2018-04-26 11:24:20 +0300

    Repository layer implementation.

---
+++ DWARF.Repository/UnitOfWork/GenericRepository.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DWARF.Core.Repository;
+using DWARF.Domain.Model;
+
+namespace DWARF.Repository.UnitOfWork
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : Base, new()
+    {
+        public bool Delete(T baseModel, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(object id, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(T baseModel, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(object id, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteMany(IEnumerable<T> baseModels, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteMany(IEnumerable<object> ids, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteManyAsync(IEnumerable<T> baseModels, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteManyAsync(IEnumerable<object> ids, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetMany(IEnumerable<string> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetMany(Expression<Func<T, bool>> filter = null, Expression<Func<IQueryable<T>, IOrderedQueryable<T>>> order = null, Expression<Func<T, object>> path = null, int skip = 0, int take = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetManyAsync(IEnumerable<string> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetManyAsync(Expression<Func<T, bool>> filter = null, Expression<Func<IQueryable<T>, IOrderedQueryable<T>>> order = null, Expression<Func<T, object>> path = null, int skip = 0, int take = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSingle(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetSingleAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Insert(T baseModel, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> InsertAsync(T baseModel, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> InsertMany(IEnumerable<T> baseModels, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> InsertManyAsync(IEnumerable<T> baseModels, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Replace(object id, T baseModel, bool persist = false, bool reset = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> ReplaceAsync(object id, T baseModel, bool persist = false, bool reset = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ReplaceMany(IEnumerable<object> ids, IEnumerable<T> baseModels, bool persist = false, bool reset = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ReplaceManyAsync(IEnumerable<object> ids, IEnumerable<T> baseModels, bool persist = false, bool reset = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T baseModel, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T baseModel, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateMany(IEnumerable<T> baseModels, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateManyAsync(IEnumerable<T> baseModels, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ready.bat
+++ ready.bat
@@ -1,5 +1,7 @@
 dotnet restore DWARF.Core
 dotnet restore DWARF.Domain
+dotnet restore DWARF.Repository
 
 dotnet build DWARF.Core
 dotnet build DWARF.Domain
+dotnet build DWARF.Repository
